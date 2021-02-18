98c4e3c5a9393719f56f02a6fea5b82ad79efba1
SavinovStepan <stepa.savinoff@gmail.com>
2021-02-18 12:11:51 +0400

    Solved task 1.1.1

---
+++ Task1/Task1.1/Task1.1.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace epamXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите первую сторону прямоугольника: ");
+                    int a = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите вторую сторону прямоугольника: ");
+                    int b = int.Parse(Console.ReadLine());
+
+                    if (a <= 0 || b <= 0)
+                    {
+                        throw new ArithmeticException("Стороны прямоугольника не могут быть меньше или равны 0!");
+                    }
+
+                    int S = a * b;
+                    Console.WriteLine("Площадь прямоугольника равна: " + S);
+                    break;
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Это не целое число!");
+                    continue;
+                }
+            }
+        }
+    }
+}
