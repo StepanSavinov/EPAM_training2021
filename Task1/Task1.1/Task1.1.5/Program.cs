f8b01ee4b5504c17b06a1deee96ab15d813a59c8
SavinovStepan <stepa.savinoff@gmail.com>
2021-02-18 14:07:10 +0400

    Solved task 1.1.5

---
+++ Task1/Task1.1/Task1.1.5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace epamXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+
+                if (i % 5 == 0 || i % 3 == 0)
+                {
+                    counter += i;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
