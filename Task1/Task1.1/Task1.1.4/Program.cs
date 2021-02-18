d75dc9a7aabc4e7991b33cb1c5a390134947291c
SavinovStepan <stepa.savinoff@gmail.com>
2021-02-18 13:57:31 +0400

    Almost solved 1.1.4

---
+++ Task1/Task1.1/Task1.1.4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace epamXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 1;
+            Console.Write("Введите кол-во треугольников: ");
+            int K = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            while (N < K)
+            {
+
+                for (int i = 0; i < N; i++)
+                {
+
+                    for (int j = 1; j < N - i; j++)
+                    {
+                        Console.Write(' ');
+                    }
+
+                    for (int j = N - 2 * i; j <= N; j++)
+                    {
+                        Console.Write('*');
+                    }
+
+                    Console.WriteLine();
+                }
+
+                N++;
+            }
+        }
+    }
+}
