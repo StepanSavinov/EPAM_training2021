b3e2a8fce842699ece671284026ad7f859473978
SavinovStepan <stepa.savinoff@gmail.com>
2021-02-18 13:05:01 +0400

    Solved task 1.1.3

---
+++ Task1/Task1.1/Task1.1.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace epamXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите основание треугольника: ");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 1; j < N - i; j++)
+                {
+                    Console.Write(' ');
+                }
+                for (int j = N - 2 * i; j <= N; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
