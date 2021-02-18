a27ac777a1e7d6aa44343fe00d12baf9b689360b
SavinovStepan <stepa.savinoff@gmail.com>
2021-02-18 12:50:42 +0400

    Solved task 1.1.2

---
+++ Task1/Task1.1/Task1.1.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace epamXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту треугольника: ");
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= N; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
