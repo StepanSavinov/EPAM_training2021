05793e6cf781baa7d70c7bd02f7cc1a34022020b
SavinovStepan <stepa.savinoff@gmail.com>
2021-03-05 18:28:54 +0400

    Solved task 1.1.8

---
+++ Task1/Task1.1/Task1.1.8/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace epamXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] arr = new int[3, 3, 3];
+            Random rand = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    for (int k = 0; k < arr.GetLength(2); k++)
+                    {
+                        arr[i, j, k] = rand.Next(-10, 10);
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    for (int k = 0; k < arr.GetLength(2); k++)
+                    {
+                        if (arr[i,j,k] > 0)
+                        {
+                            arr[i, j, k] = 0;
+                        }
+                    }
+                }
+            }
+
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    for (int k = 0; k < arr.GetLength(2); k++)
+                    {
+                        Console.Write(arr[i, j, k] + "\t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
