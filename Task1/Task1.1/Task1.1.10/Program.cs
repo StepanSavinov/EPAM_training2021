73a30851093b0ffe7996ae858a49ce3892ac8276
SavinovStepan <stepa.savinoff@gmail.com>
2021-03-07 00:12:38 +0400

    Solved tasks 1.1.9 & 1.1.10

---
+++ Task1/Task1.1/Task1.1.10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace epamXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3, 3];
+            Random rand = new Random();
+            int sum = 0;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = rand.Next(10);
+                }
+            }
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        sum += arr[i, j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i,j] + " ");
+                }
+                Console.WriteLine(Environment.NewLine);
+            }
+
+            Console.WriteLine("Сумма элементов на четных позициях: " + sum);
+        }
+    }
+}
---
+++ Task1/Task1.1/Task1.1.9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace epamXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr = new int[10];
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(-10, 10);
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    sum += arr[i];
+                }
+            }
+
+            Console.Write("Полученный массив: ");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Сумма всех неотрицательных элементов: " + sum);
+        }
+    }
+}
