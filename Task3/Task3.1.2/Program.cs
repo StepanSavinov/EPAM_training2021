35cfd293d6eceb82802f84e2aea9061f19e2d2fb
SavinovStepan <stepa.savinoff@gmail.com>
2021-04-05 19:54:33 +0400

    Solved task 3.1.2

---
+++ Task3/Task3.1.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace epamXT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var document = WordCounter(AppDomain.CurrentDomain.BaseDirectory + @"\TEXT.txt");
+
+            foreach (var pair in document)
+            {
+                Console.WriteLine("{0} : {1}", pair.Key, pair.Value + Environment.NewLine);
+            }
+        }
+
+        public static Dictionary<string, int> WordCounter(string filePath)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            var count = 0;
+
+            foreach (var line in File.ReadLines(filePath))
+            {
+                foreach (var word in line.Split(';', ',', ' ').Skip(1))
+                {
+                    result.TryGetValue(word, out count);
+                    result[word] = count + 1;
+                }
+            }
+
+            return result;
+        }
+    }
+}
