327dcb7af1c251f3e10af8290cf8a66c96d180d0
Stepan Savinov <stepa.savinoff@gmail.com>
2021-05-21 17:37:57 +0400

    Solved task 4, sorry for expired deadline :(

---
+++ Task4/epam-xt/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace epam_xt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManager manager = new FileManager();
+
+            manager.Start();
+        }
+    }
+}
